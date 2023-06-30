6e224b1fec484bac74ad96f0261834024938f88c
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-06-30 11:45:35 +0200

    Add Server.js deliver page with 2 chats Add page that delivers page with 2 chats

--- server.js
+++ server.js
@@ -44,9 +44,16 @@ app.post('/add',function(req,res){
 //app.get('/add', function(req,res){console.log(req);});
 app.get('/chat', (req, res) => {
     //res.send('Hallo Welt!');
-    console.log('kontakt zum Browser');
+    console.log('kontakt zum Browser chat page type 1');
     res.sendFile('C:/Users/peter/code/javascript/js_workshop_alfa/websocket/public/index.html');
 });
+
+app.get('/chat2', (req, res) => {
+    //res.send('Hallo Welt!');
+    console.log('kontakt zum Browser with page type 2 chats per page');
+    res.sendFile('C:/Users/peter/code/javascript/js_workshop_alfa/websocket/public/index_2_chats_on_page.html');
+});
+
 
 server.listen(3000, () => {
     console.log('server bereit auf Port 3000');
